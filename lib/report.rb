ba1f7e64213886dfc19710c1f44e1b5b76f0b375
Lilly Daniell <daniell.lilly@gmail.com>
2022-01-14 11:29:06 +0000

    Evaluates two Green tests scores

--- lib/report.rb
+++ lib/report.rb
@@ -1,7 +1,24 @@
 class Report
-  def generate_report(results)
-    if results.to_i.between?(75, 100)
-      return "Green: 1"
-    end
+
+  def initialize
+    @greens = 0
   end
+
+  def generate_report(results)
+    results_to_numbers(results)
+    
+    array_results = results.split(",").map { |number| number.to_i}
+    array_results.each do |result|
+      if result.between?(74, 100)
+        @greens += 1
+      end
+    end
+    return "Green: #{@greens}"
+  end
+
+  private 
+  def results_to_numbers(results)
+    array_results = results.split(",").map { |number| number.to_i}
+  end
+
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -1,10 +1,14 @@
 require "report"
 
 describe Report do
-  
+  let(:report) { Report.new }
+
   it "should evaluate a Green grade" do
-    report = Report.new
     expect(report.generate_report("100")).to eq("Green: 1")
+  end
+
+  it "should eveluate two Green grades" do
+    expect(report.generate_report("100, 75")).to eq("Green: 2")
   end
 
 end
